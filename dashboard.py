cb877fe6a9aa45eac063b0c4363e4729c6e94529
Dahlia Dry <dahlia@Dahlias-MacBook-Pro.local>
2023-09-06 13:21:13 +0200

    fixed live trace

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ import datetime
 
 from dashboard_components.dashboard_layout import *
 
-temp_sensor = tago.Device('20e8e742-e5e2-4c6b-acfc-8520022e5d8a')
+temp_sensor = tago.Device('16efae85-3236-4c45-a515-fa9dc1f77e90')
 buffer_length = 100
 fontawesome='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'
 mathjax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML'
@@ -27,12 +27,12 @@ def update_graph(value):
     temp2_query= {'qty':buffer_length,'variable':'temperature2'}
     result1 = temp_sensor.find(temp1_query)['result']
     result2 = temp_sensor.find(temp2_query)['result']
-    times = [result1[i]['time'] for i in range(len(result1))]
+    times = [pd.to_datetime(result1[i]['time'])+datetime.timedelta(hours=2) for i in range(len(result1))]
     temp1 = [result1[i]['value'] for i in range(len(result1))]
     temp2 = [result2[i]['value'] for i in range(len(result2))]
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=times,y=temp1,mode='lines',name='probe 1'))
-    fig.add_trace(go.Scatter(x=times,y=temp2,mode='lines',name = 'probe 2'))
+    fig.add_trace(go.Scatter(x=times,y=temp1,mode='lines',name='air sensor'))
+    fig.add_trace(go.Scatter(x=times,y=temp2,mode='lines',name = 'pipe sensor'))
     fig.update_layout(yaxis_title = 'Temperature [C]',xaxis_title='Time')
     return fig,f"*Last updated {pd.to_datetime(datetime.datetime.now()).round('1s')}*"
 
--- dashboard_components/dashboard_layout.py
+++ dashboard_components/dashboard_layout.py
@@ -6,11 +6,14 @@ LAYOUT = html.Div([
     dcc.Interval(id='update_interval',interval=5000),
     #header
     dbc.Row([
-            dbc.Col(html.Img(src='assets/logo.png',style={'width':'50%','display':'inline-block'})),
-            dbc.Col(html.H1(children='Real-time sensing dashboard',style={'display':'inline-block'}))],
-            style={'padding':'10'},className="g-0",justify="left"),
+            dbc.Col([html.Img(src='assets/logo.png',style={'width':'100%'})],width=3),
+            dbc.Col([html.H1(children='Real-time sensing dashboard')],width=9)],
+            style={'padding':10}),
     #temperature graph
     dcc.Graph(id='temp-graph'),
-    dcc.Markdown(id='last_updated',style={'padding':'10','font-size':'10px'}),
-    dcc.Markdown(children=open('assets/tempgraph_description.md').read(),style={'padding':'10'})
+    dcc.Markdown(id='last_updated',style={'padding':10,'font-size':'10px'}),
+    dbc.Row([
+        dbc.Col([html.Img(src='assets/pipe-demo.png',style={'width':'100%'})],width=4),
+        dbc.Col([dcc.Markdown(children=open('assets/tempgraph_description.md').read())],width=8)],
+        style={'padding':10}),
 ])
